dd9bf979fbf19fbde2beac33d1977b11c260b4a3
Makoto Tanabe <xekid78@outlook.com>
2018-03-27 14:30:29 +0900

    Add files via upload

---
+++ while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** ケース１ **");
+            var i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine(i + "だよ。");
+                i++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("** ケース２ **");
+            i = 5;
+            while (i >= 1)
+            {
+                Console.WriteLine(i + "だよ。");
+                i--;
+            }    
+        }
+    }
+}
